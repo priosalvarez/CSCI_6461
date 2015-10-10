3e12a3f4d011483ae557dd13f901113b3417daee
priosalvarez <odautu@gmail.com>
2015-10-10 13:40:42 -0400

    Added new instructions on case list + class and stubs for Load and Transfer Instructions

--- src/co/com/csci/util/InstructionEnum.java
+++ src/co/com/csci/util/InstructionEnum.java
@@ -5,7 +5,47 @@ public enum InstructionEnum {
 	HALT("00"),
 	LDR("01"),
 	STR("02"),
+	
+	//Load/Store Instructions
+	LDA("03"),
+	LDX("41"),	
+	STX("42"),
+	//Transfer Instructions
+	JZ("10"),
+	JNE("11"),
+	JCC("12"),
+	JMA("13"),
+	JSR("14"),
+	RFS("15"),
+	SOB("16"),
+	JGE("17"),	
+	
+	//Arithmetic and Logical Ops
+	AMR("04"),
+	SMR("05"),
+	AIR("06"),
+	SIR("07"),
+	MLT("020"),//
+	DVD("021"),
+	TRR("022"),
+	AND("023"),
+	ORR("024"),
+	NOT("025"),		
+	SRC("031"),
+	RRC("032"),
+	//Floating Point/Vector Ops
+	FADD("033"),
+	FSUB("034"),
+	VADD("035"),
+	VSUB("036"),
+	CNVRT("037"),
+	LDFR("050"),
+	STFR("051"),
 	FAULT("999");
+	
+	
+	
+	
 	
 	private String value;
 	
@@ -19,8 +59,54 @@ public enum InstructionEnum {
 				return HALT;
 			case "01":
 				return LDR;
-			case "02":
-				return STR;
+			case "02":				
+				return STR;				
+			case "10":
+				return JZ;
+			case "11":
+				return JNE;
+			case "12":
+				return JCC;
+			case "13":
+				return JMA;
+			case "14":
+				return JSR;
+			case "15":
+				return RFS;
+			case "16":
+				return SOB;
+			case "17":
+				return JGE;			
+			case "20":
+				return MLT;
+			case "21":
+				return DVD;
+			case "22":
+				return TRR;
+			case "23":
+				return AND;
+			case "24":
+				return ORR;
+			case "25":
+				return NOT;
+			case "31":
+				return SRC;
+			case "32":
+				return RRC;
+			case "33":
+				return FADD;
+			case "34":
+				return FSUB;
+			case "35":
+				return VADD;
+			case "36":
+				return VSUB;
+			case "37":
+				return CNVRT;
+			case "50":
+				return LDFR;
+			case "51":
+				return STFR;
 			default:
 				return FAULT;
 		}
--- src/csci6461/FrontPanel.java
+++ src/csci6461/FrontPanel.java
@@ -694,4 +694,123 @@ public class FrontPanel {
 		Integer eaDecimal = evaluateIndirectSTR(instruction, ea);
 		memory[eaDecimal].setText(getRegister(instruction.getRegisterNumber()));
 	}
+	
+	public void instructionLDA(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		/*Load Register with Address, r = 0..3
+				r <- EA
+				r <- c(EA), if I bit set*/
+		
+		
+	}
+	
+	public void instructionLDX(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		/*Load Index Register from Memory, x = 1..3
+				Xx <- c(EA)*/
+		
+		
+	}
+	public void instructionSTX(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		/*Store Index Register to Memory. X = 1..3
+				EA <- c(X0)
+				C(EA) <- c(Xx), if I-bit set*/
+		
+	}
+	
+	//Transfer Instructions
+	public void instructionJZ(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Jump If Zero:
+			If c(r) = 0, then PC ?? EA or c(EA), if I bit set;
+			Else PC <- PC+1*/
+		
+		
+	}
+	
+	//LOAD
+	
+	public void instructionJNE(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Jump If Not Equal:
+			If c(r) != 0, then PC ??- EA or c(EA) , if I bit set;
+			Else PC <- PC + 1*/
+		
+	}
+	public void instructionJCC(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Jump If Condition Code
+		cc replaces r for this instruction
+		cc takes values 0, 1, 2, 3 as above and specifies the bit in the Condition Code Register to check;
+		If cc bit  = 1, PC ?? EA or c(EA), if I bit set;
+		Else PC <- PC + 1*/
+		
+		
+		
+	}
+	public void instructionJMA(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Unconditional Jump To Address
+		PC <- EA, if I bit not set; PC <- c(EA), if I bit set
+		Note: r is ignored in this instruction*/
+		
+		
+	}
+	public void instructionJSR(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Jump and Save Return Address:
+			R3 <- PC+1;
+			PC <- EA    or      PC <- c(EA), if I bit set
+			R0 should contain pointer to arguments
+			Argument list should end with –17777 value*/
+		
+		
+	}
+	public void instructionRFS(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Return From Subroutine w/ return code as Immed portion (optional) stored in the instruction’s address field. 
+				R0 <- Immed; PC ?? c(R3)
+				IX, I fields are ignored.*/
+		
+		
+	}
+	public void instructionSOB(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Subtract One and Branch. R = 0..3
+				r ?? c(r) – 1
+				If c(r) > 0,  PC <- EA; but PC ?? c(EA), if I bit set;
+				Else PC <- PC + 1*/
+		
+		
+	}
+	public void instructionJGE(Instruction instruction) throws Throwable{
+		//TODO
+		String ea = "";
+		
+		/*Jump Greater Than or Equal To:
+			If c(r) >= 0, then PC <- EA or c(EA) , if I bit set;
+			Else PC <- PC + 1*/
+		
+		
+	}	
+	
+	
 }
